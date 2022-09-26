60fa7b584da95dd7eb5ed2e7764f56ee4ccfc125
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-26 02:52:15 +0200

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,24 @@
 // our-domain.com/news
 
 import React from "react";
+import Link from "next/link";
 
 function NewsPage() {
-  return <div>NewsPage</div>;
+  return (
+    <>
+      <div>NewsPage</div>;
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-important">Something Important</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
